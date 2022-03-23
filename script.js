1efbadede0a20756f9b8dea0d5becc14af370575
caique alvim <caique.apd.engenharia@gmail.com>
2022-03-23 18:26:37 -0300

    mortal

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+let sexo = prompt('digite seu sexo');
+if( sexo === "F"){
+    console.log("feminino");
+
+}else if (sexo === "M"){
+    console.log("masculino");
+
+} else {
+    console.log ("sexo invalido");
+}
